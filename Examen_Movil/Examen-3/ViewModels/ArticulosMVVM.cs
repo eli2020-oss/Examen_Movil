548cafe1ff27a56667243b805b3a8090816cffc7
User <User@DESKTOP-TO385ES>
2021-12-16 23:45:48 -0800

    Add project files.

---
+++ Examen_Movil/Examen-3/ViewModels/ArticulosMVVM.cs
@@ -0,0 +1,66 @@
+﻿using Examen_3.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+namespace Examen_3.ViewModels
+{
+    public class ArticulosMVVM
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public ArticulosMVVM(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Operacion>().Wait();
+        }
+
+        public ArticulosMVVM()
+        {
+        }
+
+        public Task<List<Operacion>> GetTaskAsync()
+        {
+            return _database.Table<Operacion>().OrderByDescending(x => x.Fecha).ToListAsync();
+        }
+
+        
+        public Task<int> SaveTaskAsync(Operacion taskModel)
+        {
+
+            if (taskModel.Id_pago != 0)
+            {
+                return _database.UpdateAsync(taskModel);
+            }
+            else
+            {
+                return _database.InsertAsync(taskModel);
+            }
+
+        }
+
+        public Task<int>UpdateTaskAsync(Operacion pagos)
+        {
+            return _database.UpdateAsync(pagos);
+        }
+
+        public Task<int> deleteAsync(Operacion pagos)
+        {
+            return _database.DeleteAsync(pagos);
+        }
+
+        public Task<Operacion> GetItemAsync(int personId)
+        {
+            return _database.Table<Operacion>().Where(i => i.Id_pago == personId).FirstOrDefaultAsync();
+        }
+
+        public Task<Operacion> DeleteItemAsync(int personId)
+        {
+            return _database.Table<Operacion>().Where(i => i.Id_pago == personId).FirstOrDefaultAsync();
+        }
+
+    }
+}
---
+++ Examen_Movil/Examen-3/Views/ModificarOperaciones.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Examen_3;
+using Examen_3.Models;
+using Examen_3.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen_Movil.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModificarOperaciones : ContentPage
+    {
+        public ModificarOperaciones()
+        {
+            InitializeComponent();
+        }
+        private async void btneliminar_Clicked(object sender, EventArgs e)
+        {
+            var _pagos = new Operacion
+            {
+
+                Id_pago = Convert.ToInt32(this.idpago.Text),
+                Descripcion = this.description.Text,
+                Monto = Convert.ToDouble(this.monto.Text),
+                Fecha = this.DueDate.Date,
+
+
+
+
+
+
+
+
+            };
+
+            if (await App.BaseDatos.deleteAsync(_pagos) != 0)
+            {
+                await DisplayAlert("Eliminar Persona", "Persona Eliminada Correctamente", "Ok");
+                await Navigation.PushModalAsync(new ListaPagos());
+
+            }
+            else
+                await DisplayAlert("Eliminar Persona", "Error al Eliminar Persona!!", "Ok");
+            //await DisplayAlert // Convert.ToDateTime( this.DueDate.no),
+
+
+        }
+
+        private async void btnactualizar_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(idpago.Text))
+            {
+                Operacion person = new Operacion()
+                {
+                    Id_pago = Convert.ToInt32(idpago.Text),
+                    Descripcion = description.Text,
+                    Monto = Convert.ToInt32(monto.Text)
+
+                };
+
+                //Update Person  
+                await App.BaseDatos.SaveTaskAsync(person);
+
+                idpago.Text = string.Empty;
+                description.Text = string.Empty;
+
+                await DisplayAlert("Alert", "Pago Actualizado Correctamente", "OK");
+
+            }
+            else
+            {
+                await DisplayAlert("Required", "Please Enter PersonID", "OK");
+            }
+        }
+         private async void btnlista_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new ListaPagos());
+        }
+    }
+}
